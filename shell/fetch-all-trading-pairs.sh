44667e746bba9291cb1f65f18c26f9d9a72e4f24
aisling11 <19312514+aisling11@users.noreply.github.com>
2021-10-07 17:35:11 +0100

    improve script comment and update symbols.txt

--- shell/fetch-all-trading-pairs.sh
+++ shell/fetch-all-trading-pairs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# simple scripts to download all trading symbols
+# Simple script to download all symbols from https://api.binance.com/api/v3/exchangeInfo
 # jq is required
 
 curl -s -H 'Content-Type: application/json'  https://api.binance.com/api/v3/exchangeInfo | jq -r '.symbols | sort_by(.symbol) | .[] | .symbol' > symbol.txt
