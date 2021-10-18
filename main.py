ccb6f8aea88ba3fd0925eefc09c048a8fb4351ce
miao.lin <miao.lin@envision-digital.com>
2021-10-18 10:05:20 +0800

    unzip file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import wget
+import zipfile
 import pandas as pd
 
 from datetime import datetime
@@ -20,7 +21,12 @@ if __name__ == "__main__":
     end_date_dt = datetime.strptime(end_date, "%Y-%m-%d")
     date_list = pd.date_range(start_date_dt, end_date_dt, freq="1D")
     for date in date_list:
-        file = "{}-{}-{}.zip".format(symbol, temporal, date.strftime("%Y-%m-%d"))
-        url = "https://data.binance.vision/data/futures/um/daily/markPriceKlines/{}/{}/{}".format(symbol, temporal, file)
+        zip_file = "{}-{}-{}.zip".format(symbol, temporal, date.strftime("%Y-%m-%d"))
+        url = "https://data.binance.vision/data/futures/um/daily/markPriceKlines/{}/{}/{}".format(symbol, temporal, zip_file)
         print(url)
         wget.download(url, data_folder_path)
+
+        zip_file_path = os.path.join(data_folder_path, zip_file)
+        if os.path.exists(zip_file_path):
+            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+                zip_ref.extractall(data_folder_path)
