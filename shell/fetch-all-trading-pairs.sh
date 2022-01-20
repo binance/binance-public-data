06307b4c94297f8e1a0d07d3507708b2918836b0
Doug Ransom <doug.ransom@gmail.com>
2022-01-20 12:23:09 -0800

    removing shell

--- shell/fetch-all-trading-pairs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Simple script to download all symbols from https://api.binance.com/api/v3/exchangeInfo
-# jq is required
-
-curl -s -H 'Content-Type: application/json'  https://api.binance.com/api/v3/exchangeInfo | jq -r '.symbols | sort_by(.symbol) | .[] | .symbol' > symbol.txt
