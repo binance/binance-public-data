4e903bbef767a99f15a82cf08fb3cff81f1965ef
Jeremy <365354+2pd@users.noreply.github.com>
2022-05-12 22:14:39 +1000

    add missing 3m interval in shell script (#142)

--- shell/download-klines.sh
+++ shell/download-klines.sh
@@ -3,8 +3,8 @@
 # This is a simple script to download klines by given parameters.
 
 symbols=("BNBUSDT" "BTCUSDT") # add symbols here to download
-intervals=("1m" "5m" "15m" "30m" "1h" "2h" "4h" "6h" "8h" "12h" "1d" "3d" "1w" "1mo")
-years=("2017" "2018" "2019" "2020")
+intervals=("1m" "3m" "5m" "15m" "30m" "1h" "2h" "4h" "6h" "8h" "12h" "1d" "3d" "1w" "1mo")
+years=("2017" "2018" "2019" "2020" "2021" "2022")
 months=(01 02 03 04 05 06 07 08 09 10 11 12)
 
 baseurl="https://data.binance.vision/data/spot/monthly/klines"
