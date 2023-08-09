9ab1cc704716e5fc3c5892a72cb2ce64388c5bcd
李彦辰 Li Yanchen <25627435+BoogalooLi@users.noreply.github.com>
2023-08-09 12:03:12 +0800

    Update fetch-all-trading-pairs.sh (#256)
    add cm futures and um futures symbol download scripts

--- shell/fetch-all-trading-pairs.sh
+++ shell/fetch-all-trading-pairs.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Simple script to download all symbols from https://api.binance.com/api/v3/exchangeInfo
+# Simple script to download all symbols from https://api.binance.com/api/v3/exchangeInfo https://dapi.binance.com/dapi/v1/exchangeInfo
 # jq is required
 
-curl -s -H 'Content-Type: application/json'  https://api.binance.com/api/v3/exchangeInfo | jq -r '.symbols | sort_by(.symbol) | .[] | .symbol' > symbol.txt
+# spot
+curl -s -H 'Content-Type: application/json'  https://api.binance.com/api/v3/exchangeInfo | jq -r '.symbols | sort_by(.symbol) | .[] | .symbol' > spot_symbol.txt
+# cm futures
+curl -s -H 'Content-Type: application/json'  https://dapi.binance.com/dapi/v1/exchangeInfo | jq -r '.symbols | sort_by(.symbol) | .[] | .symbol' > cm_symbol.txt
+# um futures
+curl -s -H 'Content-Type: application/json'  https://fapi.binance.com/fapi/v1/exchangeInfo | jq -r '.symbols | sort_by(.symbol) | .[] | .symbol' > um_symbol.txt
