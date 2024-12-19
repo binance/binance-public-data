f6a4d47a0d710d57441a9df74b03b85c88cdc6cd
cloudQuant <yunjinqi@gmail.com>
2024-12-19 15:39:15 +0800

    update donwload daily kline to multi-threaded

--- python/download-kline.py
+++ python/download-kline.py
@@ -13,7 +13,7 @@ import pandas as pd
 from enums import *
 from utility import download_file, get_all_symbols, get_parser, get_start_end_date_objects, convert_to_date_object, \
     get_path
-
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def download_monthly_klines(trading_type, symbols, num_symbols, intervals, years, months, start_date, end_date, folder,
                             checksum):
@@ -55,6 +55,43 @@ def download_monthly_klines(trading_type, symbols, num_symbols, intervals, years
         current += 1
 
 
+# def download_daily_klines(trading_type, symbols, num_symbols, intervals, dates, start_date, end_date, folder, checksum):
+#     current = 0
+#     date_range = None
+#
+#     if start_date and end_date:
+#         date_range = start_date + " " + end_date
+#
+#     if not start_date:
+#         start_date = START_DATE
+#     else:
+#         start_date = convert_to_date_object(start_date)
+#
+#     if not end_date:
+#         end_date = END_DATE
+#     else:
+#         end_date = convert_to_date_object(end_date)
+#
+#     #Get valid intervals for daily
+#     intervals = list(set(intervals) & set(DAILY_INTERVALS))
+#     print("Found {} symbols".format(num_symbols))
+#
+#     for symbol in symbols:
+#         print("[{}/{}] - start download daily {} klines ".format(current + 1, num_symbols, symbol))
+#         for interval in intervals:
+#             for date in dates:
+#                 current_date = convert_to_date_object(date)
+#                 if current_date >= start_date and current_date <= end_date:
+#                     path = get_path(trading_type, "klines", "daily", symbol, interval)
+#                     file_name = "{}-{}-{}.zip".format(symbol.upper(), interval, date)
+#                     download_file(path, file_name, date_range, folder)
+#
+#                     if checksum == 1:
+#                         checksum_path = get_path(trading_type, "klines", "daily", symbol, interval)
+#                         checksum_file_name = "{}-{}-{}.zip.CHECKSUM".format(symbol.upper(), interval, date)
+#                         download_file(checksum_path, checksum_file_name, date_range, folder)
+#
+#         current += 1
 def download_daily_klines(trading_type, symbols, num_symbols, intervals, dates, start_date, end_date, folder, checksum):
     current = 0
     date_range = None
@@ -72,26 +109,45 @@ def download_daily_klines(trading_type, symbols, num_symbols, intervals, dates, 
     else:
         end_date = convert_to_date_object(end_date)
 
-    #Get valid intervals for daily
+    # Get valid intervals for daily
     intervals = list(set(intervals) & set(DAILY_INTERVALS))
     print("Found {} symbols".format(num_symbols))
 
     for symbol in symbols:
         print("[{}/{}] - start download daily {} klines ".format(current + 1, num_symbols, symbol))
-        for interval in intervals:
-            for date in dates:
-                current_date = convert_to_date_object(date)
-                if current_date >= start_date and current_date <= end_date:
-                    path = get_path(trading_type, "klines", "daily", symbol, interval)
-                    file_name = "{}-{}-{}.zip".format(symbol.upper(), interval, date)
-                    download_file(path, file_name, date_range, folder)
 
-                    if checksum == 1:
-                        checksum_path = get_path(trading_type, "klines", "daily", symbol, interval)
-                        checksum_file_name = "{}-{}-{}.zip.CHECKSUM".format(symbol.upper(), interval, date)
-                        download_file(checksum_path, checksum_file_name, date_range, folder)
+        # Create a ThreadPoolExecutor
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = []
+
+            for interval in intervals:
+                for date in dates:
+                    current_date = convert_to_date_object(date)
+                    if current_date >= start_date and current_date <= end_date:
+                        # Submit the download task to the thread pool
+                        futures.append(
+                            executor.submit(download_file_task, trading_type, symbol, interval, date, date_range,
+                                            folder, checksum))
+
+            # Wait for all tasks to complete
+            for future in as_completed(futures):
+                try:
+                    future.result()
+                except Exception as e:
+                    print(f"Error occurred while downloading: {e}")
 
         current += 1
+
+
+def download_file_task(trading_type, symbol, interval, date, date_range, folder, checksum):
+    path = get_path(trading_type, "klines", "daily", symbol, interval)
+    file_name = "{}-{}-{}.zip".format(symbol.upper(), interval, date)
+    download_file(path, file_name, date_range, folder)
+
+    if checksum == 1:
+        checksum_path = get_path(trading_type, "klines", "daily", symbol, interval)
+        checksum_file_name = "{}-{}-{}.zip.CHECKSUM".format(symbol.upper(), interval, date)
+        download_file(checksum_path, checksum_file_name, date_range, folder)
 
 
 if __name__ == "__main__":
