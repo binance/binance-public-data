de6bdec7520bcae888d66c362257f120dfc9d65b
alplabin <122352306+alplabin@users.noreply.github.com>
2025-01-09 18:34:13 +0900

    Microsecond update

--- shell/download-klines.sh
+++ shell/download-klines.sh
@@ -4,7 +4,7 @@
 
 symbols=("BNBUSDT" "BTCUSDT") # add symbols here to download
 intervals=("1m" "3m" "5m" "15m" "30m" "1h" "2h" "4h" "6h" "8h" "12h" "1d" "3d" "1w" "1mo")
-years=("2017" "2018" "2019" "2020" "2021" "2022" "2023" "2024")
+years=("2017" "2018" "2019" "2020" "2021" "2022" "2023" "2024" "2025")
 months=(01 02 03 04 05 06 07 08 09 10 11 12)
 
 baseurl="https://data.binance.vision/data/spot/monthly/klines"
